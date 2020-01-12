50436afe5d4d855f3b99c7771431f62ddf189357
Miguel Moreno <miguelazomm@gmail.com>
2020-01-12 16:44:00 -0400

    login con email funcionando

--- seguridad/admin.py
+++ seguridad/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Planta, Proceso, Tag, Estandar
+from .models import Planta, Proceso, Tag, Estandar, User
 
 # Register your models here.
 admin.site.register(Planta)
 admin.site.register(Estandar)
 admin.site.register(Proceso)
 admin.site.register(Tag)
+admin.site.register(User)
 
 
